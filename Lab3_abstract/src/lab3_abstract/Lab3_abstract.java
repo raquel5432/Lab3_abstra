04be40827d333576cf9bea7ccd3e46ec6ed06a29
ALISSON RAQUEL MARTINEZ SERRANO <alisson_martinez@unitec.edu>
2026-02-06 08:50:57 -0600

    o

---
+++ Lab3_abstract/src/lab3_abstract/Lab3_abstract.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab3_abstract;
+
+/**
+ *
+ * @author ALISSONRAQUELMARTINE
+ */
+public class Lab3_abstract {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
